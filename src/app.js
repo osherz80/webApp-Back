7c73f96644cf92afc648a04ab4d4e39c91858e78
Osher <osherzohar80@gmail.com>
2026-01-03 19:52:18 +0200

    first servef init

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Basic Route
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Could not connect to MongoDB', err);
+    });
