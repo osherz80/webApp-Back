c00ba719c6691c739a3e9d6f6e99426f82b093eb
Osher <osherzohar80@gmail.com>
2026-01-04 20:14:37 +0200

    safeguard api's with the middleware

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import postController from '../controllers/postController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
-router.post('/', postController.addPost);
+router.post('/', authMiddleware, postController.addPost);
 router.get('/', postController.getAllPosts);
 router.get('/:id', postController.getPostById);
-router.put('/:id', postController.updatePost);
+router.put('/:id', authMiddleware, postController.updatePost);
 
 export default router;
