3cf4b76f45808d4e952174a2e2f82a4c1c2be0b9
osherz80 <88611368+osherz80@users.noreply.github.com>
2026-01-04 11:43:08 +0200

    Assingment 1 (#11)
    * first servef init
    
    * add posts schema and first api
    
    * get all posts with sender filter option
    
    * added getPostById endpoint
    
    * add update post api
    
    * added requests file
    
    * init comment model
    
    * add comment api
    
    * add get all comments api
    
    * add get comment by id api
    
    * add update comment by id
    
    * add delete comment api
    
    * create rest test file
    
    * delete select all comments when there is not post id parameter
    
    ---------
    
    Co-authored-by: Assaf Revacha <assaf.git.arm@gmail.com>
    Co-authored-by: Assaf Revacha <assaf.revacha@gmail.com>
    Co-authored-by: Assafof1 <79972225+Assafof1@users.noreply.github.com>

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+const postRoutes = require('./routes/postRoutes');
+app.use('/post', postRoutes);
+
+const commentRoutes = require('./routes/commentRoutes');
+app.use('/comments', commentRoutes);
+
+// Basic Route
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Could not connect to MongoDB', err);
+    });
+    
