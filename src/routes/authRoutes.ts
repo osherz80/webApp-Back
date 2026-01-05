3587acfdbf9d993473523e5e977823dc3c876afe
Osher <osherzohar80@gmail.com>
2026-01-05 14:00:58 +0200

    add back refresh

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -5,5 +5,6 @@ import authController from '../controllers/authController.js';
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
+router.post('/refresh', authController.refresh);
 
 export default router;
