c1278e9451c58dc03dd71e7e8fbd601da15a94d9
Assaf Revacha <assaf.git.arm@gmail.com>
2026-03-08 20:48:49 +0200

    remove unnecessary js files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -11,26 +11,26 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Routes
-const postRoutes = require('./routes/postRoutes');
-app.use('/post', postRoutes);
+const postRoutes = require("./routes/postRoutes");
+app.use("/post", postRoutes);
 
-const commentRoutes = require('./routes/commentRoutes');
-app.use('/comments', commentRoutes);
+const commentRoutes = require("./routes/commentRoutes");
+app.use("/comments", commentRoutes);
 
 // Basic Route
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
-
+console.log("1");
 // Database Connection
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('Connected to MongoDB');
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        });
-    })
-    .catch((err) => {
-        console.error('Could not connect to MongoDB', err);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
-    
+  })
+  .catch((err) => {
+    console.error("Could not connect to MongoDB", err);
+  });
